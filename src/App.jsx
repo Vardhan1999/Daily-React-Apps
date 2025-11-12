9fd13c2865a4d5d488c5a68aadca1c5ea7ba8e8c
Vardhan Jain <vardhanjaindit@gmail.com>
2025-11-12 11:29:07 +0530

    Todo Lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
 import React, { useState } from "react";
 
 export default function App() {
-    const [active, setActive] = useState("tab1");
+    const [text, setText] = useState("");
+    const [todos, setTodos] = useState([]);
+
+    function addTodos() {
+        if (!text.trim) return;
+        setTodos([...todos, { id: crypto.randomUUID(), text, completed: false }]);
+        setText("");
+    }
+
+    function toggleTodo(id) {
+        setTodos(todos.map(todo => (todo.id === id ? { ...todo, completed: !todo.completed } : todo)));
+    }
+
+    function deleteTodo(id) {
+        setTodos(todos.filter(todo => todo.id !== id));
+    }
 
     return (
-        <div>
-            <button onClick={() => setActive("tab1")}>Tab 1</button>
-            <button onClick={() => setActive("tab2")}>Tab 2</button>
-
-            {active === "tab1" && <p>Content of Tab</p>}
-            {active === "tab2" && <p>Content of Tab 2</p>}
-        </div>
+        <>
+            <h2>Todo Lists!</h2>
+            <input value={text} onChange={(e) => setText(e.target.value)} />
+            <button onClick={addTodos}>Add Todo</button>
+            <ul>
+                {todos.map(todo => (
+                    <li key={todo.id}>
+                        <input type="checkbox" checked={todo.completed} onChange={() => toggleTodo(todo.id)} />
+                        {todo.text}
+                        <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+                    </li>
+                ))}
+            </ul>
+        </>
     )
 }
