86946d67c3c37ffb5f250923fb68687fae622808
Vardhan Jain <vardhanjaindit@gmail.com>
2025-12-01 22:59:53 +0530

    Counter App

--- .history/src/App_20251201225758.jsx
+++ .history/src/App_20251201225758.jsx
@@ -7,8 +7,6 @@ export default function App() {
         <div>
             <h2>Counter:{count}</h2>
             <button onClick={() => setCount(count + 1)}>Increment</button>
-            <button onClick={() => setCount(count - 1)} disabled={count === 0}>Decrement</button>
-            <button onClick={() => setCount(0)}>Reset</button>
         </div>
     )
 }
