3b8c4e56296f64b83c1c53e90272ea3f87857252
Vardhan Jain <vardhanjaindit@gmail.com>
2025-12-08 23:28:50 +0530

    Tabs Component

--- .history/src/App_20251208232600.jsx
+++ .history/src/App_20251208232600.jsx
@@ -7,10 +7,6 @@ export default function App() {
         <div>
             <button onClick={() => setActive("tab1")}>Tab 1</button>
             <button onClick={() => setActive("tab2")}>Tab 2</button>
-            <div>
-                {active === "tab1" && <p>Content of Tab 1</p>}
-                {active === "tab2" && <p>Content of Tab 2</p>}
-            </div>
         </div>
     )
 }
